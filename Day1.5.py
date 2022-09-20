5824f0207df778a018739e64e47949a196049e80
SESHA SAI BHATTIPROLU <112627809+SESHASAIBHATTIPROLU@users.noreply.github.com>
2022-09-20 09:27:30 +0530

    Add files via upload

---
+++ Day1.5.py
@@ -0,0 +1,9 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/SESHA/AppData/Local/Programs/Python/Python310/Day1.6.py ==
+True
+False
+False
+True
+True
