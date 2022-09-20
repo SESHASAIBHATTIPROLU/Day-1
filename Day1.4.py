01efb28f4dba92472db076b466f4e43c0eba4d49
SESHA SAI BHATTIPROLU <112627809+SESHASAIBHATTIPROLU@users.noreply.github.com>
2022-09-20 09:46:50 +0530

    Delete Day1.4.py

--- Day1.4.py
+++
@@ -1,6 +0,0 @@
-Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-=== RESTART: C:/Users/SESHA/AppData/Local/Programs/Python/Python310/Day1.4.py ==
-enter the number : 1
-Yes
